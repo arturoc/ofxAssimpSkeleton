ab6314ef5005d754782a0321880c74d53fcffe2d
arturo <arturo@openframeworks.cc>
2011-10-28 18:12:16 +0200

    first commit

---
+++ src/testApp.cpp
@@ -0,0 +1,214 @@
+#include "testApp.h"
+
+aiMatrix4x4 & toAi(ofMatrix4x4 & m){
+	return *((aiMatrix4x4*)&m);
+}
+
+ofMatrix4x4 randomRotation(float amt = 180) {
+	ofQuaternion quat;
+	ofMatrix4x4 mat;
+	ofVec3f axis(ofRandomf(), ofRandomf(), ofRandomf());
+	axis.normalize();
+	quat.makeRotate(ofRandom(-amt, amt), axis);
+	quat.get(mat);
+	return mat;
+}
+
+ofBoundingBox::ofBoundingBox(){
+
+}
+
+ofBoundingBox::ofBoundingBox(const ofVec3f & _min, const ofVec3f & _max){
+	set(_min,_max);
+}
+
+void ofBoundingBox::set(const ofVec3f & _min, const ofVec3f & _max){
+	min = _min;
+	max = _max;
+	mesh.clear();
+	mesh.setMode(OF_PRIMITIVE_TRIANGLES);
+	mesh.addVertex(min);
+	mesh.addVertex(ofVec3f(max.x,min.y,min.z));
+	mesh.addVertex(ofVec3f(max.x,min.y,max.z));
+	mesh.addVertex(ofVec3f(min.x,min.y,max.z));
+
+	mesh.addVertex(ofVec3f(min.x,max.y,min.z));
+	mesh.addVertex(ofVec3f(max.x,max.y,min.z));
+	mesh.addVertex(max);
+	mesh.addVertex(ofVec3f(min.x,max.y,max.z));
+
+	mesh.addIndex(0);
+	mesh.addIndex(1);
+	mesh.addIndex(2);
+
+	mesh.addIndex(2);
+	mesh.addIndex(3);
+	mesh.addIndex(0);
+
+	mesh.addIndex(0);
+	mesh.addIndex(1);
+	mesh.addIndex(4);
+
+	mesh.addIndex(4);
+	mesh.addIndex(5);
+	mesh.addIndex(1);
+
+	mesh.addIndex(1);
+	mesh.addIndex(2);
+	mesh.addIndex(5);
+
+	mesh.addIndex(5);
+	mesh.addIndex(6);
+	mesh.addIndex(2);
+
+	mesh.addIndex(2);
+	mesh.addIndex(3);
+	mesh.addIndex(6);
+
+	mesh.addIndex(6);
+	mesh.addIndex(7);
+	mesh.addIndex(3);
+
+	mesh.addIndex(3);
+	mesh.addIndex(0);
+	mesh.addIndex(7);
+
+	mesh.addIndex(7);
+	mesh.addIndex(4);
+	mesh.addIndex(0);
+
+	mesh.addIndex(4);
+	mesh.addIndex(5);
+	mesh.addIndex(6);
+
+	mesh.addIndex(6);
+	mesh.addIndex(7);
+	mesh.addIndex(5);
+}
+
+void testApp::randomPose() {
+	Pose pose = model.getPose();
+	for(Pose::iterator i = pose.begin(); i != pose.end(); i++) {
+		aiMatrix4x4& cur = i->second;
+		ofMatrix4x4 mat = randomRotation(45);
+		cur.a1 = mat(0, 0);
+		cur.a2 = mat(0, 1);
+		cur.a3 = mat(0, 2);
+		cur.b1 = mat(1, 0);
+		cur.b2 = mat(1, 1);
+		cur.b3 = mat(1, 2);
+		cur.c1 = mat(2, 0);
+		cur.c2 = mat(2, 1);
+		cur.c3 = mat(2, 2);
+	}
+	model.pose(pose);
+	//curRotation = randomRotation();
+}
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	model.loadModel("humanfem2.dae");
+	ofBackground(0,0,0);
+
+	//cout << "numBones:" << model.getNumBones();
+
+    ofEnableBlendMode(OF_BLENDMODE_ALPHA);
+	glEnable(GL_DEPTH_TEST);
+	/*ofMatrix4x4 matrix;
+	model.*/
+	randomPose();
+	model.calculateSkeleton();
+	body = model.getCurrentAnimatedMesh(0);
+	body.clearColors();
+	ofEnableLighting();
+    glShadeModel(GL_SMOOTH);
+    light.enable();
+    //light.setSpotlight();
+    ofEnableSeparateSpecularLight();
+
+    wireframe = false;
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+
+	ofTranslate(ofGetWidth()*0.5,ofGetHeight()*0.5);
+	ofRotateX((float(mouseY)/float(ofGetHeight())*2.f-1.f)*180.f);
+	ofRotateY((float(mouseX)/float(ofGetWidth())*2.f-1.f)*180.f);
+
+	ofTranslate(model.getPosition());
+
+	ofRotate(180, 0, 0, 1);
+	ofTranslate(-model.getSceneCenter().x, -model.getSceneCenter().y, model.getSceneCenter().z);
+
+	ofScale(model.getNormalizedScale() , model.getNormalizedScale(), model.getNormalizedScale());
+
+	//ofScale(scale.x, scale.y, scale.z);
+
+	if(wireframe){
+		body.drawWireframe();
+		for(int i=0;i<(int)model.skeleton.size();i++){
+			model.skeleton[i].mesh.drawFaces();
+		}
+
+	}else{
+		body.drawFaces();
+		for(int i=0;i<(int)model.skeleton.size();i++){
+			model.skeleton[i].mesh.drawWireframe();
+		}
+	}
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+	if(key=='w'){
+		wireframe = !wireframe;
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+	randomPose();
+	model.calculateSkeleton();
+	body = model.getCurrentAnimatedMesh(0);
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/testApp.h
@@ -0,0 +1,131 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxAssimpModelLoader.h"
+
+
+class ofBoundingBox{
+public:
+	ofBoundingBox();
+	ofBoundingBox(const ofVec3f & min, const ofVec3f & max);
+
+	void set(const ofVec3f & min, const ofVec3f & max);
+
+	ofVec3f min;
+	ofVec3f max;
+	ofMesh mesh;
+};
+
+typedef map<string, aiMatrix4x4> Pose;
+class RiggedModel : public ofxAssimpModelLoader {
+public:
+	Pose getPose(int which = 0) {
+		const aiMesh* mesh = modelMeshes[which].mesh;
+		int n = mesh->mNumBones;
+		Pose pose;
+		for(int a = 0; a < n; a++) {
+			const aiBone* bone = mesh->mBones[a];
+			aiNode* node = scene->mRootNode->FindNode(bone->mName);
+			pose[node->mName.data] = aiMatrix4x4();
+		}
+		return pose;
+	}
+	void pose(Pose& pose, int which = 0) {
+		const aiMesh* mesh = modelMeshes[which].mesh;
+		int n = mesh->mNumBones;
+		vector<aiMatrix4x4> boneMatrices(n);
+		for(int a = 0; a < n; a++) {
+			const aiBone* bone = mesh->mBones[a];
+			aiNode* node = scene->mRootNode->FindNode(bone->mName);
+			boneMatrices[a] = bone->mOffsetMatrix;
+			const aiNode* tempNode = node;
+			while(tempNode) {
+				aiMatrix4x4 curPose = pose[tempNode->mName.data];
+				boneMatrices[a] = (tempNode->mTransformation * curPose) * boneMatrices[a];
+				tempNode = tempNode->mParent;
+			}
+			modelMeshes[which].hasChanged = true;
+			modelMeshes[which].validCache = false;
+		}
+
+		modelMeshes[which].animatedPos.assign(modelMeshes[which].animatedPos.size(),0);
+		if(mesh->HasNormals()){
+			modelMeshes[which].animatedNorm.assign(modelMeshes[which].animatedNorm.size(),0);
+		}
+
+		// loop through all vertex weights of all bones
+		for(int a = 0; a < n; a++) {
+			const aiBone* bone = mesh->mBones[a];
+			const aiMatrix4x4& posTrafo = boneMatrices[a];
+			for( size_t b = 0; b < bone->mNumWeights; b++) {
+				const aiVertexWeight& weight = bone->mWeights[b];
+				size_t vertexId = weight.mVertexId;
+				const aiVector3D& srcPos = mesh->mVertices[vertexId];
+				modelMeshes[which].animatedPos[vertexId] += weight.mWeight * (posTrafo * srcPos);
+			}
+			if(mesh->HasNormals()) {
+				// 3x3 matrix, contains the bone matrix without the translation, only with rotation and possibly scaling
+				aiMatrix3x3 normTrafo = aiMatrix3x3( posTrafo);
+				for( size_t b = 0; b < bone->mNumWeights; b++) {
+					const aiVertexWeight& weight = bone->mWeights[b];
+					size_t vertexId = weight.mVertexId;
+					const aiVector3D& srcNorm = mesh->mNormals[vertexId];
+					modelMeshes[which].animatedNorm[vertexId] += weight.mWeight * (normTrafo * srcNorm);
+				}
+			}
+		}
+		updateGLResources();
+	}
+
+	void calculateSkeleton(int which=0){
+		skeleton.clear();
+		const aiMesh* mesh = modelMeshes[which].mesh;
+		int n = mesh->mNumBones;
+		for(int a = 0; a < n; a++) {
+			const aiBone* bone = mesh->mBones[a];
+			ofVec3f min(9999,9999,9999),max(-9999,-9999,-9999);
+			for( size_t b = 0; b < bone->mNumWeights; b++) {
+				const aiVertexWeight& weight = bone->mWeights[b];
+				size_t vertexId = weight.mVertexId;
+				const aiVector3D & tmp = modelMeshes[which].animatedPos[vertexId];
+				min.x = MIN(min.x,tmp.x);
+				min.y = MIN(min.y,tmp.y);
+				min.z = MIN(min.z,tmp.z);
+
+				max.x = MAX(max.x,tmp.x);
+				max.y = MAX(max.y,tmp.y);
+				max.z = MAX(max.z,tmp.z);
+			}
+			skeleton.push_back(ofBoundingBox(min,max));
+		}
+	}
+
+	vector<ofBoundingBox> skeleton;
+};
+
+
+
+class testApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void randomPose();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+		RiggedModel model;
+		ofVboMesh body;
+		ofLight light;
+		bool wireframe;
+};
